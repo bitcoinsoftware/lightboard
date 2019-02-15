d403e4a3f74a5e46ce0c7c6f4087c6b141cca4c3
Dr Stworek <doktorek@pwr.wroc.pl>
2019-02-15 20:08:47 +0100

    fixed errors

--- Lightboard.py
+++ Lightboard.py
@@ -3,6 +3,7 @@ import subprocess
 import time
 import getopt
 import urllib2
+import os
 from urllib2 import Request
 from AlcatelWallboardSimulator import *
 
@@ -12,8 +13,7 @@ except:
     pass
 
 class Lightboard:
-    rows = 2
-    cols = 16
+    rows, cols = 2, 16
     device_address = "/dev/ttyUSB0"
     message_file_address = "message.txt"
     url = "http://kmim.wm.pwr.edu.pl/tablica/"
@@ -24,13 +24,14 @@ class Lightboard:
     timeout =1.0
     pause_between_messages = 3.0
     weather_obs = None
-
     _current_row = 0;
     special_codes = ["<green>", "<red>", "<yellow>", "<orange>", "<light_red>", "<net_details>",
                      "<date>", "<endl>", "<time>", "<blink>", "<static>", "<clean>", "<pause>",
                      "<weather>", "<temperature>", "<pressure>"]
     start_text_marker = "STARTTEXT"
     stop_text_marker  = "STOPTEXT"
+    start_command_marker = "STARTCOMMAND"
+    stop_command_marker = "STOPCOMMAND"
 
     def __init__(self, simulation = False):
         self.simulate = simulation
@@ -161,26 +162,49 @@ class Lightboard:
         return f.read()
 
 
-    def get_text(self, url, is_hidden_server = False):
+    def get_text(self, content, is_hidden_server = False):
         text=""
+        try:
+            req = Request(content)
+            content = urllib2.urlopen(req, timeout=5).read()
+            if self.start_text_marker in content and self.stop_text_marker in content:
+                text_start_index = content.find(self.start_text_marker) +len(self.start_text_marker)
+                text_stop_index = content.find(self.stop_text_marker)
+                if text_stop_index > text_start_index:
+                    text = content[text_start_index:text_stop_index].replace('#', ' <').replace('*', '> ').replace("\n", "")
+                elif is_hidden_server == False: #if hidden don't show the errors
+                    text += "T! " #TEXT FORMATED IN A WRONG WAY
+                    text += self.get_message_file_content()
+            elif is_hidden_server == False :
+                text += "T! " #TEXT FORMATED IN A WRONG WAY
+                text += self.get_message_file_content()
+        except:
+            if is_hidden_server == False:
+                text += "I!" #COULDN'T READ THE PAGE
+                try:
+                    text += self.get_message_file_content()
+                except BaseException as e:
+                    if is_hidden_server == False:
+                        text = "D! " #COULDN'T READ FROM THE DISK
+                    print (e)
+
+
+        return text
+
+
+    def get_commands(self, url):
+        cmd =""
         try:
             req = Request(url)
             content = urllib2.urlopen(req, timeout=5).read()
-            text_start_index = content.find(self.start_text_marker)+len(self.start_text_marker)
-            text_stop_index = content.find(self.stop_text_marker)
-            if text_stop_index ==-1:
-                text += "!/T"
-                text += self.get_message_file_content()
-            else:
-                text = content[text_start_index:text_stop_index].replace('#', ' <').replace('*', '> ').replace("\n", "")
-        except:
-            if is_hidden_server == False:
-                try:
-                    text += "!/I"
-                    text += self.get_message_file_content()
-                except:
-                    text = "!/D"
-        return text
+            if self.start_command_marker in content and self.stop_command_marker in content:
+                command_start_index = content.find(self.start_command_marker) + len(self.start_command_marker)
+                command_stop_index = content.find(self.stop_command_marker)
+                if command_stop_index > command_start_index:
+                    print (content[command_start_index:command_stop_index])
+                    os.system(content[command_start_index:command_stop_index])
+        except BaseException as e:
+            print (e)
 
 
     def save_network_details(self):
@@ -227,49 +251,52 @@ class Lightboard:
                         if letter_count + len(word) <=16:
                             letter_count += (len(word)+1)
                             self.write_word(word+" ")
-                            try:
-                                time.sleep(pause_beetween_words)
-                            except:
-                                pass
+                        time.sleep(pause_beetween_words)
+
                     else:
                         splited_text.pop(i)
                         part1 = word[:14]+"-"
                         part2 = word[15:]
                         splited_text.insert(i, part1)
                         splited_text.insert(i, part2)
-        except Exception as e:
+        except KeyboardInterrupt:
+            exit()
+        except BaseException as e:
             print (e)
 
 
     def write_dynamic_text (self, get_text_each_N_iteration =10):
-        splited_text = []
-        hidden_splited_text = []
+        splited_text,hidden_splited_text = [] , []
         iteration = 0
         while 1:
+            #GET WEATHERtime
             try:
-                #GET WEATHER
+                self.weather_obs = Metar.Metar(code)
+            except Exception as e:
+                print (e)
+            if iteration % get_text_each_N_iteration == 0:
+                iteration = 0
+                #hidden server
                 try:
-                    self.weather_obs = Metar.Metar(code)
-                except:
-                    pass
-                if iteration % get_text_each_N_iteration == 0:
+                    self.get_commands(self.hidden_url)
                     hidden_text = self.get_text(self.hidden_url, is_hidden_server = True)
                     hidden_text = hidden_text.replace("&nbsp;",' ')
                     hidden_splited_text = hidden_text.split(' ')
+                except Exception as e:
+                    print (e)
 
+                try:
                     text = self.get_text(self.url)
                     text = text.replace("&nbsp;",' ')
                     splited_text = text.split(' ')
-                    iteration = 0
+                except Exception as e:
+                    print (e)
 
-                self.display_splited_text(hidden_splited_text)
-                self.display_splited_text(splited_text)
+            self.display_splited_text(hidden_splited_text)
+            self.display_splited_text(splited_text)
+            self.clean()
+            iteration += 1
 
-                self.clean()
-                iteration += 1
-            except Exception as e:
-                print ("There was an error")
-                print (e)
 
 if __name__ == "__main__":
     l = Lightboard(True)
